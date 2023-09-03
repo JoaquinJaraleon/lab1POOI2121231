cefb70f194f2e5ff1f89312db69fb598c90728ff
JoaquinJaraleon <142684268+JoaquinJaraleon@users.noreply.github.com>
2023-09-02 22:56:51 -0500

    Add files via upload

---
+++ Ejercicio11.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio.poo1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        int num = 3;
+              if(num > 0 && num%2!=0) {
+                  System.out.println("true");
+              }
+    }
+}
---
+++ Ejercicio12.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio.poo1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        int numA = 4, numB = 8;
+              if(numA > 0 && numB > 0) {
+                  System.out.println("true");
+              }
+    }
+}
