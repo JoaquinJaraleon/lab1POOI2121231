25576eab999bc01c826099422a60d26b028dd6a0
JoaquinJaraleon <142684268+JoaquinJaraleon@users.noreply.github.com>
2023-09-02 22:56:01 -0500

    Add files via upload

---
+++ Ejercicio13.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio.poo1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        int numA = 7, numB = 5;
+              if(numA > 0 && numB > 0) {
+                  System.out.println("true");
+              }
+    }
+}
